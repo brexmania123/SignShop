ecb6ac3c5e4c908115d21fad2f5d4758a58741cd
Brentie2006 <31364333+Brentie2006@users.noreply.github.com>
2018-03-02 10:06:50 +0100

    fix the problems

--- src/SignShop/SignShop.php
+++ src/SignShop/SignShop.php
@@ -13,7 +13,7 @@
  * @link http://xionbig.netsons.org/plugins/SignShop 
  * @link http://forums.pocketmine.net/plugins/signshop.668/
  * @description Buy and Sell the items using Signs with virtual-money.
- * @version 1.12.0
+ * @version 3.0.0-ALPHA11
  * @api 1.11.0
 
 namespace SignShop;
